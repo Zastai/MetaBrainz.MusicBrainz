b202cab1fd57adbf31a7932d2f3631926855d368
Tim Van Holder <tim.vanholder@gmail.com>
2016-04-28 19:43:18 +0200

    Added AssemblyInformationalVersion assembly attributes to all projects.

--- MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
+++ MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
@@ -11,5 +11,9 @@ using System.Runtime.InteropServices;
 
 // This should be MAJOR.MINOR, matching libmusicbrainz.
 [assembly: AssemblyVersion    ("5.1")]
+
 // This should be MAJOR.MINOR.PATCHLEVEL, with MAJOR.MINOR matching libmusicbrainz and PATCHLEVEL as a release sequence number (not following libmusicbrainz).
 [assembly: AssemblyFileVersion("5.1.0")]
+
+// This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
+[assembly: AssemblyInformationalVersion("5.1.0 (pre-release)")]
