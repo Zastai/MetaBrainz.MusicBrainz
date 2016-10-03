f18765b0301d77ec26cc5cb5060767a757265a3b
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-03 20:13:27 +0200

    Implement ITextResource in TextEntity.

--- MetaBrainz.MusicBrainz/Model/TextEntity.cs
+++ MetaBrainz.MusicBrainz/Model/TextEntity.cs
@@ -1,12 +1,20 @@
 ﻿using System.Xml.Serialization;
 
+using MetaBrainz.MusicBrainz.Resources;
+
 namespace MetaBrainz.MusicBrainz.Model {
 
-  public abstract class TextEntity : MbEntity {
+  public abstract class TextEntity : MbEntity, ITextResource {
 
     #region XML Elements
 
     [XmlText] public string Text;
+
+    #endregion
+
+    #region ITextResource
+
+    string ITextResource.Text => this.Text;
 
     #endregion
 
