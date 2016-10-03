e23acdf70a067e19e6dac844f3b72cef31a255c6
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-03 20:11:40 +0200

    Added Iswc class. Needed to be able to make IswcList an IResourceList<ITextResource>.

---
+++ MetaBrainz.MusicBrainz/Model/Iswc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Xml.Serialization;
+
+using MetaBrainz.MusicBrainz.Resources;
+
+namespace MetaBrainz.MusicBrainz.Model {
+
+  [Serializable]
+  public class Iswc : Item, ITextResource {
+
+    #region XML Elements
+
+    [XmlText] public string Text;
+
+    #endregion
+
+    #region ITextResource
+
+    string ITextResource.Text => this.Text;
+
+    #endregion
+
+  }
+
+}
--- MetaBrainz.MusicBrainz/Model/Lists/IswcList.cs
+++ MetaBrainz.MusicBrainz/Model/Lists/IswcList.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
+
+using MetaBrainz.MusicBrainz.Resources;
 
 namespace MetaBrainz.MusicBrainz.Model.Lists {
 
   [Serializable]
-  public class IswcList : ItemList {
+  public class IswcList : ItemList, IResourceList<ITextResource> {
 
-    [XmlElement("iswc")] public string[] Items;
+    [XmlElement("iswc")] public Iswc[] Items;
+
+    #region IResourceList<ITextResource>
+
+    uint? IResourceList<ITextResource>.Count => this.ListCount;
+
+    uint? IResourceList<ITextResource>.Offset => this.ListOffset;
+
+    IEnumerable<ITextResource> IResourceList<ITextResource>.Items => this.Items;
+
+    #endregion
 
   }
 
