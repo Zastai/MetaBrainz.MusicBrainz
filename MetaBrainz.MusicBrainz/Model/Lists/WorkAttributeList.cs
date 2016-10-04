bac381ca3cf6e40fb96412594cd669c74fe9e53a
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-04 20:37:10 +0200

    Implemented some resource interfaces.

--- MetaBrainz.MusicBrainz/Model/Lists/WorkAttributeList.cs
+++ MetaBrainz.MusicBrainz/Model/Lists/WorkAttributeList.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
+
+using MetaBrainz.MusicBrainz.Resources;
 
 namespace MetaBrainz.MusicBrainz.Model.Lists {
 
   [Serializable]
-  public class WorkAttributeList : Item {
+  public class WorkAttributeList : Item, IResourceList<IWorkAttribute> {
 
     [XmlElement("attribute")] public WorkAttribute[] Items;
+
+    #region IResourceList<IWorkAttribute>
+
+    uint? IResourceList<IWorkAttribute>.Count => (this.Items == null) ? null : (uint?) this.Items.Length;
+
+    uint? IResourceList<IWorkAttribute>.Offset => null;
+
+    IEnumerable<IWorkAttribute> IResourceList<IWorkAttribute>.Items => this.Items;
+
+    #endregion
 
   }
 
