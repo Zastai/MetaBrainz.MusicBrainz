d302171dcb84d3bc19d5127c64fe73325c6fe4fe
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-04 21:42:59 +0200

    Bumped assembly version to match the NuGet spec.

--- MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
+++ MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
@@ -9,8 +9,8 @@ using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion    ("0.1")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion    ("0.2")]
+[assembly: AssemblyFileVersion("0.2.0")]
 
 // This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
-[assembly: AssemblyInformationalVersion("0.1.0 (pre-release)")]
+[assembly: AssemblyInformationalVersion("0.2.0 (pre-release)")]
