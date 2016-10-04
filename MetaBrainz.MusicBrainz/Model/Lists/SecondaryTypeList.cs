09edc1499f0f8f4e3c89354aed0c3acbc3b61394
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-04 20:38:25 +0200

    Implemented some resource interfaces.

--- MetaBrainz.MusicBrainz/Model/Lists/SecondaryTypeList.cs
+++ MetaBrainz.MusicBrainz/Model/Lists/SecondaryTypeList.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
+
+using MetaBrainz.MusicBrainz.Resources;
 
 namespace MetaBrainz.MusicBrainz.Model.Lists {
 
   [Serializable]
-  public class SecondaryTypeList : Item {
+  public class SecondaryTypeList : Item, IResourceList<ITextResource> {
 
     [XmlElement("secondary-type")] public SecondaryType[] Items;
+
+    #region Implementation of IResourceList<out ITextResource>
+
+    uint? IResourceList<ITextResource>.Count  => (this.Items == null) ? null : (uint?) this.Items.Length;
+
+    uint? IResourceList<ITextResource>.Offset => null;
+
+    IEnumerable<ITextResource> IResourceList<ITextResource>.Items => this.Items;
+
+    #endregion
 
   }
 
