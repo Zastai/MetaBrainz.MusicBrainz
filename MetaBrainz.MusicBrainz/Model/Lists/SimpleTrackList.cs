74ac31e5a0ec747999ee57ea27d4b321fc1f9e38
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-04 20:38:01 +0200

    Implemented some resource interfaces.

--- MetaBrainz.MusicBrainz/Model/Lists/SimpleTrackList.cs
+++ MetaBrainz.MusicBrainz/Model/Lists/SimpleTrackList.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Xml.Serialization;
+
+using MetaBrainz.MusicBrainz.Resources;
 
 namespace MetaBrainz.MusicBrainz.Model.Lists {
 
   [Serializable]
-  public class SimpleTrackList : ItemList {
+  public class SimpleTrackList : ItemList, IResourceList<ISimpleTrackInfo> {
 
     [XmlElement("track")] public SimpleTrackInfo[] Items;
+
+    #region IResourceList<ISimpleTrackInfo>
+
+    uint? IResourceList<ISimpleTrackInfo>.Count => this.ListCount;
+
+    uint? IResourceList<ISimpleTrackInfo>.Offset => this.ListOffset;
+
+    IEnumerable<ISimpleTrackInfo> IResourceList<ISimpleTrackInfo>.Items => this.Items;
+
+    #endregion
 
   }
 
