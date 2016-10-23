8c04d1ed5631a93b7829790bc36552d043d0458f
Tim Van Holder <tim.vanholder@gmail.com>
2016-10-23 16:35:37 +0200

    Set MusicBrainz version to 0.2.0.

--- MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
+++ MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
@@ -15,4 +15,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("0.2.0")]
 
 // This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
-[assembly: AssemblyInformationalVersion("0.2.0 (pre-release)")]
+[assembly: AssemblyInformationalVersion("0.2.0")]
