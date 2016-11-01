8e46e2e79645de8bc4160f034a2370882d52ef10
Tim Van Holder <tim.vanholder@gmail.com>
2016-11-01 18:28:38 +0100

    IRelationship: Added docs.

--- MetaBrainz.MusicBrainz/Entities/IRelationship.cs
+++ MetaBrainz.MusicBrainz/Entities/IRelationship.cs
@@ -3,34 +3,49 @@ using System.Collections.Generic;
 
 namespace MetaBrainz.MusicBrainz.Entities {
 
+  /// <summary>A relationship between two entities.</summary>
   public interface IRelationship {
 
+    /// <summary>The attributes attached to the relationship (if any). These values are keys into <see cref="AttributeValues"/>.</summary>
     IEnumerable<string> Attributes { get; }
 
+    /// <summary>The attribute values attached to the relationship (if any).</summary>
     IDictionary<string, string> AttributeValues { get; }
 
+    /// <summary>The date the relationship began.</summary>
     string Begin { get; }
 
+    /// <summary>The direction of the relationship (forward/backward).</summary>
     string Direction { get; }
 
+    /// <summary>The date the relationship ended.</summary>
     string End { get; }
 
+    /// <summary>Flag indicating whether or not the relationship has ended.</summary>
     bool? Ended { get; }
 
+    /// <summary>An optional ordering key for the relationships.</summary>
     int? OrderingKey { get; }
 
+    /// <summary>An optional alternate name for the source of the relationship.</summary>
     string SourceCredit { get; }
 
+    /// <summary>The target of the relationship.</summary>
     IMbEntity Target { get; }
 
+    /// <summary>An optional alternate name for the target of the relationship.</summary>
     string TargetCredit { get; }
 
+    /// <summary>The type of entity targeted by the relationship (as an enumeration value).</summary>
     EntityType TargetType { get; }
 
+    /// <summary>The type of entity targeted by the relationship (as text).</summary>
     string TargetTypeText { get; }
 
+    /// <summary>The relationship type.</summary>
     string Type { get; }
 
+    /// <summary>The internal ID for the relationship type, if applicable.</summary>
     Guid? TypeId { get; }
 
   }
