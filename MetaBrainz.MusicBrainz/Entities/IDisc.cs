77c2b4d1cd3c4f98e7967cad6333c249444f211b
Tim Van Holder <tim.vanholder@gmail.com>
2016-11-01 18:20:39 +0100

    IDisc: dropped IEntity, added docs.

--- MetaBrainz.MusicBrainz/Entities/IDisc.cs
+++ MetaBrainz.MusicBrainz/Entities/IDisc.cs
@@ -2,14 +2,22 @@
 
 namespace MetaBrainz.MusicBrainz.Entities {
 
-  public interface IDisc : IEntity {
+  /// <summary>A compact disc.</summary>
+  public interface IDisc {
 
+    /// <summary>The Musicbrainz disc ID for this disc.</summary>
+    string Id { get; }
+
+    /// <summary>The number of offsets set for this disc.</summary>
     int OffsetCount { get; }
 
+    /// <summary>The offsets (Red Book sector addresses) for this disc.</summary>
     IEnumerable<int> Offsets { get; }
 
+    /// <summary>The releases that include this disc.</summary>
     IEnumerable<IRelease> Releases { get; }
 
+    /// <summary>The total size of this disc, in Red Book sectors.</summary>
     int Sectors { get; }
 
   }
