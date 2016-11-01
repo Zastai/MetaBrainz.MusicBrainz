59a7c60159874546261c36e2a910a579d378d27f
Tim Van Holder <tim.vanholder@gmail.com>
2016-11-01 17:54:30 +0100

    IWorkAttribute: dropped ITypedEntity and added docs.

--- MetaBrainz.MusicBrainz/Entities/IWorkAttribute.cs
+++ MetaBrainz.MusicBrainz/Entities/IWorkAttribute.cs
@@ -2,10 +2,19 @@
 
 namespace MetaBrainz.MusicBrainz.Entities {
 
-  public interface IWorkAttribute : ITypedEntity {
+  /// <summary>An attribute set for a work (<see cref="IWork"/>).</summary>
+  public interface IWorkAttribute {
 
+    /// <summary>The type of the attribute.</summary>
+    string Type { get; }
+
+    /// <summary>The internal ID for the attribute type.</summary>
+    Guid? TypeId { get; }
+
+    /// <summary>The value for the attribute.</summary>
     string Value { get; }
 
+    /// <summary>The internal ID for the value, if applicable.</summary>
     Guid? ValueId { get; }
 
   }
