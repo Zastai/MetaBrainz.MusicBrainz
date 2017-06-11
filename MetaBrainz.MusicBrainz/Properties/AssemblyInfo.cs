9e6d38a2c6ba50e5c4b0a234f9d6d7345212edcc
Tim Van Holder <tim.vanholder@gmail.com>
2017-06-11 17:31:34 +0200

    Set up NuGet packaging.
    Dropped the nuget tree and most attributes from AssemblyInfo.cs, in favour of settings in MSBuild.
    The DiscId library is now CLS compliant.

--- MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
+++ MetaBrainz.MusicBrainz/Properties/AssemblyInfo.cs
@@ -1,18 +1,5 @@
 ﻿using System;
-using System.Reflection;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle        ("MetaBrainz.MusicBrainz")]
-[assembly: AssemblyDescription  (".NET Implementation of libmusicbrainz")]
-[assembly: AssemblyCompany      ("MetaBrainz")]
-[assembly: AssemblyProduct      ("libmusicbrainz")]
-[assembly: AssemblyCopyright    ("Copyright © Tim Van Holder 2016")]
 
 [assembly: CLSCompliant(true)]
 [assembly: ComVisible(false)]
-
-[assembly: AssemblyVersion    ("0.3")]
-[assembly: AssemblyFileVersion("0.3.0")]
-
-// This should match AssemblyFileVersion, plus optional extra tags (such as a pre-release indicator).
-[assembly: AssemblyInformationalVersion("0.3.0 (pre-release)")]
