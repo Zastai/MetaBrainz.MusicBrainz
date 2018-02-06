5ca7ca8e3a466cf733bedf1de15636ba5859abc2
Tim Van Holder <tim.vanholder@gmail.com>
2018-02-06 23:32:25 +0100

    Added an enumeration of the HTTP methods used by the MusicBrainz API.

---
+++ MetaBrainz.MusicBrainz/Method.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace MetaBrainz.MusicBrainz {
+
+  /// <summary>Enumeration of the supported request methods.</summary>  
+  [SuppressMessage("ReSharper", "InconsistentNaming")]
+  internal enum Method {
+    /// <summary>HTTP DELETE: Delete a resource.</summary>
+    DELETE,
+    /// <summary>HTTP GET: Request data from a resource.</summary>
+    GET,
+    /// <summary>HTTP POST: Submit data to a resource.</summary>
+    POST,
+    /// <summary>HTTP PUT: Upload data representing a resource.</summary>
+    PUT,
+  }
+
+}
