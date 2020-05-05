3c940bee035cc9a9553930b3c8c690e9d2f61e91
Tim Van Holder <tim.vanholder@gmail.com>
2020-05-05 23:37:56 +0200

    Tweak NameCredit.ToString().

--- MetaBrainz.MusicBrainz/Objects/Entities/NameCredit.cs
+++ MetaBrainz.MusicBrainz/Objects/Entities/NameCredit.cs
@@ -13,8 +13,10 @@ namespace MetaBrainz.MusicBrainz.Objects.Entities {
 
     public override string ToString() {
       var text = string.Empty;
-      if (this.Artist != null && this.Artist.Name != this.Name)
+      if (this.Artist != null && this.Name != null && this.Artist.Name != this.Name)
         text += $"[{this.Artist} as “{this.Name}”]";
+      else if (this.Artist != null)
+        text += this.Artist.ToString();
       else if (this.Name != null)
         text += this.Name;
       if (this.JoinPhrase != null)
